a63dcf7fab3fa2c06a20852d74080cb552ac8ab9
Atakan Colak <atakan.colak@softwire.com>
2024-01-31 12:54:57 +0000

    Remove required keyword

--- Challenges/Employee.cs
+++ Challenges/Employee.cs
@@ -7,31 +7,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Employee
 {
     [Key]
-    public required long Id { get; init; }
-    
+    public long Id { get; init; }
+
     [Column("name")]
-    public required string Name { get; set; }
-    
+    public string Name { get; set; }
+
     [Column("age")]
-    public required long Age { get; set; }
-    
+    public long Age { get; set; }
+
     [Column("job_title")]
-    public required string JobTitle { get; set; }
-    
+    public string JobTitle { get; set; }
+
     [Column("department")]
     public string? Department { get; set; }
-    
+
     [Column("manager_id")]
     public long? ManagerId { get; set; }
-    
+
     [Column("years_of_service")]
-    public required long YearsOfService { get; set; }
-    
+    public long YearsOfService { get; set; }
+
     [Column("pay_band")]
-    public required long PayBand { get; set; }
-    
+    public long PayBand { get; set; }
+
     [Column("on_leave")]
-    public required long OnLeave { get; set; }
+    public long OnLeave { get; set; }
 
     public override string ToString()
     {
